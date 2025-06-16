51bab315a1358a5a37235f00e6dda139d187116c
ahmed ihab <ahmedehab28001@gmail.com>
2025-06-16 16:42:12 +0300

    Modified the arch/Method extension for IService Collection/Serilog and FluentValdiation

---
+++ src/Application/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            var assembly = typeof(DependencyInjection).Assembly;
+            services.AddValidatorsFromAssembly(assembly);
+            return services;
+        }
+    }
+}
