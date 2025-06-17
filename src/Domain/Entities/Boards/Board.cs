debdf1c652fdb79f173bfabeb91630c8d31465e6
ahmed ihab <ahmedehab28001@gmail.com>
2025-06-18 02:11:53 +0300

    Adding Entities and Common Folder in Domain with Board Entity and BaseEntity classes

---
+++ src/Domain/Entities/Boards/Board.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Boards
+{
+    public sealed class Board : BaseEntity<Guid>
+    {
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public Board(string title, string? description = null)
+        {
+            if (string.IsNullOrWhiteSpace(title))
+                throw new ArgumentException("Title is required");
+            Id = Guid.NewGuid();
+            Title = title;
+            Description = description;
+            CreatedAt = DateTime.Now;
+        }
+
+    }
+}
