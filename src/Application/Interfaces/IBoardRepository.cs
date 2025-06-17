fa2edc9c25903914171c75cc3f3520a39edab5dc
ahmed ihab <ahmedehab28001@gmail.com>
2025-06-18 02:39:01 +0300

    feat: add IBoardRepository interface with CRUD operations

---
+++ src/Application/Interfaces/IBoardRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities.Boards;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IBoardRepository
+    {
+        public void Add(Board board);
+        public Board? GetById(Guid id);
+        public IEnumerable<Board> GetAll();
+        public void Update (Board board);
+        public void Delete(Guid id);
+    }
+}
