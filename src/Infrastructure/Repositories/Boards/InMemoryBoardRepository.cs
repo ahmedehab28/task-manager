f5d8b4a3fca14dbddb68044fc1e97fbbfd1386dd
ahmed ihab <ahmedehab28001@gmail.com>
2025-06-18 03:00:30 +0300

    feat: add InMemoryBoardRepository to support memory storage for Boards

---
+++ src/Infrastructure/Repositories/Boards/InMemoryBoardRepository.cs
@@ -0,0 +1,38 @@
+﻿using Application.Interfaces;
+using Domain.Entities.Boards;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories.Boards
+{
+    public class InMemoryBoardRepository : IBoardRepository
+    {
+        public void Add(Board board)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Board> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Board? GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Board board)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
