08c29ac46b49f814cad4650709f6aa6d1223b63e
ahmed ihab <ahmedehab28001@gmail.com>
2025-06-24 14:48:08 +0300

    test(CreateBoard): add test cases for CreateBoardHandler and CreateBoardValidator

---
+++ tests/Application.Tests/Boards/Commands/CreateBoard/CreateBoardHandlerTests.cs
@@ -0,0 +1,45 @@
+﻿using Application.Boards.Commands.CreateBoard;
+using Application.Interfaces;
+using Domain.Entities.Boards;
+using FluentAssertions;
+using Moq;
+
+namespace Application.Tests.Boards.Commands.CreateBoard
+{
+    public class CreateBoardHandlerTests
+    {
+        private readonly Mock<IBoardRepository> _mockRepo;
+        private readonly CreateBoardHandler _handler;
+        public CreateBoardHandlerTests()
+        {
+            _mockRepo = new Mock<IBoardRepository>();
+            _handler = new CreateBoardHandler(_mockRepo.Object);
+            
+        }
+
+        [Fact]
+        public async Task CreateBoardHandler_WithValidInput_ShouldCreateBoard()
+        {
+            // Arrange
+            string boardTitle = "Board Title";
+            string boardDescription = "Board Description";
+            var cmd = new CreateBoardCommand(boardTitle, boardDescription);
+            Board saved = null!;
+            var setup = _mockRepo
+                .Setup(r => r.Add(It.IsAny<Board>()))
+                .Callback<Board>(board => saved = board);
+
+            // Act
+            var id = await _handler.Handle(cmd, CancellationToken.None);
+
+            // Assertion
+            id.Should().NotBe(Guid.Empty);
+            saved.Should().NotBeNull();
+            saved.Id.Should().Be(id);
+            saved.Title.Should().Be(boardTitle);
+            saved.Description.Should().Be(boardDescription);
+
+        }
+
+    }
+}
---
+++ tests/Application.Tests/Boards/Commands/CreateBoard/CreateBoardValidatorTests.cs
@@ -0,0 +1,54 @@
+﻿
+using Application.Boards.Commands.CreateBoard;
+using FluentValidation.TestHelper;
+
+namespace Application.Tests.Boards.Commands.CreateBoard
+{
+    public class CreateBoardValidatorTests
+    {
+        private readonly CreateBoardValidator _validator;
+
+        public CreateBoardValidatorTests()
+        {
+            _validator = new CreateBoardValidator();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("    ")]
+        public void CreateBoardValidtaor_WithInvalidInput_ShouldFailValidation(string? title)
+        {
+            // Arrange
+            var cmd = new CreateBoardCommand(title!, "Board Description");
+
+            // Act
+            var result = _validator.TestValidate(cmd);
+
+            // Assertion
+            result.ShouldHaveValidationErrorFor(x => x.Title);
+
+        }
+
+        [Theory]
+        [InlineData("1")]
+        [InlineData("T")]
+        [InlineData("#")]
+        [InlineData("T1#")]
+        [InlineData("Board Title")]
+        public void CreateBoardValidator_WithValidInput_ShouldPassValidation(string title)
+        {
+            // Arrange
+            var cmd = new CreateBoardCommand(title, "Description");
+
+            // Act
+            var result = _validator.TestValidate(cmd);
+
+            // Assertion
+            result.ShouldNotHaveValidationErrorFor(x => x.Title);
+        }
+
+        
+    }
+}
