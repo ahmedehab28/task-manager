78c04ea8354a823d6951c2698b37d4eee171806e
ahmed ihab <ahmedehab28001@gmail.com>
2025-06-29 01:59:34 +0300

    feat(CreateBoard): add Create method in BoardsController in Api Layer

---
+++ src/WebApi/Controllers/BoardsController.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Application.Boards.Commands.CreateBoard;
+using Asp.Versioning;
+using Application.Boards.DTOs;
+
+[ApiController]
+[ApiVersion("1")]
+[Route("api/v{version:apiVersion}/[controller]")]
+public class BoardsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    public BoardsController(IMediator mediator) => _mediator = mediator;
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateBoardDto request)
+    {
+        var cmd = new CreateBoardCommand(request.Title, request.Description);
+        var id = await _mediator.Send(cmd);
+        return CreatedAtAction(nameof(Create), new { id }, new { id });
+    }
+
+}
