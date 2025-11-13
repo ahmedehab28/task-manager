e80f6041d4837752165674dc41875d6c2318438b
ahmed ihab <ahmedehab28001@gmail.com>
2025-11-13 17:49:41 +0200

    feat(serialization): add JsonConverter for Optional<T> to support partial updates

---
+++ src/Application/Common/Models/Optional.cs
@@ -0,0 +1,16 @@
+﻿namespace Application.Common.Models
+{
+    public readonly struct Optional<T>
+    {
+        private readonly T _value; 
+        public bool IsAssigned { get; }
+        public T Value => IsAssigned ? _value : throw new InvalidOperationException("No value present.");
+        public Optional(T? value) 
+        {
+            IsAssigned = true;
+            _value = value!;
+        }
+        public static implicit operator Optional<T>(T value) => new Optional<T>(value); 
+        public override string ToString() => IsAssigned ? (Value?.ToString() ?? "null") : "<absent>";
+    }
+}
